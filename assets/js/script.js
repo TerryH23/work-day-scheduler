061a1a00dfd02f5513c6411c7522f01dc1eb3f44
Terry <terryh23@gmail.com>
2022-09-17 02:02:13 -0700

    added timeblocks and some javascript function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,46 @@
-var jumbo = $(".jumbotron").css("background-color", "lightblue");
+$(document).ready(function(){
 
-var containerEl = $(".container");
+$(".jumbotron").css("background-color", "lightblue");
 
-var today = moment();
-$("#currentDay").text(today.format("dddd LL"));
 
+var today = moment().format("dddd LL hA")
+$("#currentDay").text(today);
+
+
+
+// need to fix the function, it is only adding the future class
+
+$(".time-block").each(function() {
+
+    var currentTime = moment().format("hA");
+    var hour = $(".hour");
+    
+    if (currentTime == hour) {
+        $(this).addClass("present");
+        // $(this).removeClass("past");
+    
+    } else if (currentTime < hour) {
+        $(this).removeClass("present");
+        $(this).addClass("future");
+    
+    } else if (currentTime > hour) {
+        $(this).removeClass("future");
+        $(this).addClass("past");
+    }
+})
+
+
+
+// still need to add save button feature and localstorage
+
+
+// $(".saveBtn").click(function (event) {
+//     event.preventDefault;
+//     $("<textarea>").val("");
+//     localStorage.setItem("")
+// })
+
+
+
+
+});
